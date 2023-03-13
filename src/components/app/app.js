7600f9f896ec687e41c54069b3049de3f7cb24f9
Ayur Tushinov <tushinov.ayur@gmail.com>
2023-03-13 11:15:11 +0800

    feat: add deleteemployee funcitonality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,49 @@
+import { Component } from 'react/cjs/react.production.min';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 import './app.css';
 
-function App() {
-  return (
-    <div className="app">
-      <AppInfo/>
+class App extends Component {
 
-      <div className="search-panel">
-        <SearchPanel/>
-        <AppFilter/>
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John', salary: 800, increase: false, id: 1},
+        {name: 'Alex', salary: 3000, increase: true, id: 2},
+        {name: 'Karl', salary: 5000, increase: false, id: 3}
+      ]
+    }
+  }
+
+  deleteItem = (id) => {
+    this.setState(({data}) => {    
+      return {
+        data: data.filter(item => item.id !== id),
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo/>
+  
+        <div className="search-panel">
+          <SearchPanel/>
+          <AppFilter/>
+        </div>
+        <EmployeesList data={this.state.data}
+        onDelete={this.deleteItem}/>
+        <EmployeesAddForm/>
       </div>
-      <EmployeesList/>
-      <EmployeesAddForm/>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
